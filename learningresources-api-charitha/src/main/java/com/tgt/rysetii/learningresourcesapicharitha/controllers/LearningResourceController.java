ca282e2249904d3810fdaa0f1c9c4efd787a3eb0
charitha918 <charitha16214@gmail.com>
2022-07-20 19:50:41 +0530

    Added Rest Controllers

---
+++ learningresources-api-charitha/src/main/java/com/tgt/rysetii/learningresourcesapicharitha/controllers/LearningResourceController.java
@@ -0,0 +1,29 @@
+package com.tgt.rysetii.learningresourcesapicharitha.controllers;
+
+import com.tgt.rysetii.learningresourcesapicharitha.entity.LearningResource;
+import com.tgt.rysetii.learningresourcesapicharitha.service.LearningResourceService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class LearningResourceController {
+    LearningResourceService service;
+
+    public LearningResourceController(LearningResourceService service)
+    {this.service=service;}
+    @GetMapping("/details")
+    public List<LearningResource> getDetails(){
+        return service.getLearningResources();
+    }
+    @PostMapping(value="/details", consumes="application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void saveDetails(@RequestBody List<LearningResource> lrs){
+        service.saveLearningResources(lrs);
+    }
+    @DeleteMapping(value="/details/{id}")
+    public void deleteDetails(@PathVariable int id){
+        service.deleteLearningResourceById(id);
+    }
+}
